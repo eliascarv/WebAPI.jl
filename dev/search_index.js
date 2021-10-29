4cf976fc382db5e0d8e028441aed3cb0d7bf0e7e
Documenter.jl <documenter@juliadocs.github.io>
2021-10-29 00:24:46 +0000

    build based on 9d71784

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = WebAPI","category":"page"},{"location":"#WebAPI","page":"Home","title":"WebAPI","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for WebAPI.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [WebAPI]","category":"page"}]
+}
